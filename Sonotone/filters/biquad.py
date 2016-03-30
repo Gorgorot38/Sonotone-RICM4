d002a05661538e01b0af2d37b577409b4c2f10fa
HATTINGUAIS Julian <jultuning@gmail.com>
2016-03-30 16:54:14 +0200

    Modification des parametres des filtres

--- Sonotone/filters/biquad.py
+++ Sonotone/filters/biquad.py
@@ -5,8 +5,7 @@ Created on Wed Mar 30 09:32:23 2016
 @author: Julian
 
  TODO:
-     Corriger les parametres associe aux filtres
-     Ne prendre que ceux utiles
+     Doc des filtres
 """
 
 from type import TypeFilter as F
@@ -24,7 +23,7 @@ class _Biquad():
                  a0 + a1*z^-1 + b1*z^-2
     """
 
-    def __init__(self, type, freq, dbGain, srate, bandwidthOrQOrS, isBandwidth):
+    def __init__(self, type, freq, dbGain, bandwidthOrQOrS, srate=44100., isBandwidth=False):
 
         if (type == F.PEAKING or type == F.LOW_SHELF or type == F.HIGH_SHELF):
             self.A = pow(10, dbGain / 40.)
@@ -66,8 +65,8 @@ class _Biquad():
 
 class Lowpass(_Biquad):
 
-    def __init__(self, freq, dbGain, srate, bandwidthOrQOrS, isBandwidth):
-        _Biquad.__init__(self,F.LOW_PASS,freq,dbGain,srate,bandwidthOrQOrS,isBandwidth)
+    def __init__(self, freq, bandwidthOrQOrS, srate=44100, isBandwidth=False):
+        _Biquad.__init__(self,F.LOW_PASS,freq,0.0,bandwidthOrQOrS,srate,isBandwidth)
 
         b0 = (1. - self.cs) / 2.
         b1 = 1. - self.cs
@@ -80,8 +79,8 @@ class Lowpass(_Biquad):
 
 class Highpass(_Biquad):
 
-    def __init__(self, freq, dbGain, srate, bandwidthOrQOrS, isBandwidth):
-        _Biquad.__init__(self,F.HIGH_PASS,freq,dbGain,srate,bandwidthOrQOrS,isBandwidth)
+    def __init__(self, freq, bandwidthOrQOrS, srate=44100, isBandwidth=False):
+        _Biquad.__init__(self,F.HIGH_PASS,freq,0.0,bandwidthOrQOrS,srate,isBandwidth)
 
         b0 = (1. +  self.cs) / 2.
         b1 = -(1. + self.cs)
@@ -94,8 +93,8 @@ class Highpass(_Biquad):
 
 class Bandpass(_Biquad):
 
-    def __init__(self, freq, dbGain, srate, bandwidthOrQOrS, isBandwidth):
-        _Biquad.__init__(self,F.BAND_PASS,freq,dbGain,srate,bandwidthOrQOrS,isBandwidth)
+    def __init__(self, freq, bandwidthOrQOrS, srate=44100, isBandwidth=False):
+        _Biquad.__init__(self,F.BAND_PASS,freq,0.0,bandwidthOrQOrS,srate,isBandwidth)
 
         b0 = self.alpha
         b1 = 0.
@@ -108,8 +107,8 @@ class Bandpass(_Biquad):
 
 class Notch(_Biquad):
 
-    def __init__(self, freq, dbGain, srate, bandwidthOrQOrS, isBandwidth):
-        _Biquad.__init__(self,F.NOTCH,freq,dbGain,srate,bandwidthOrQOrS,isBandwidth)
+    def __init__(self, freq, bandwidthOrQOrS, srate=44100, isBandwidth=False):
+        _Biquad.__init__(self,F.NOTCH,freq,0.0,bandwidthOrQOrS,srate,isBandwidth)
 
         b0 = 1.
         b1 = -2. * self.cs
@@ -123,8 +122,8 @@ class Notch(_Biquad):
 
 class Allpass(_Biquad):
 
-    def __init__(self, freq, dbGain, srate, bandwidthOrQOrS, isBandwidth):
-        _Biquad.__init__(self,F.ALL_PASS,freq,dbGain,srate,bandwidthOrQOrS,isBandwidth)
+    def __init__(self, freq, bandwidthOrQOrS, isBandwidth, srate=44100):
+        _Biquad.__init__(self,F.ALL_PASS,freq,0.0,bandwidthOrQOrS,isBandwidth,srate)
 
         b0 = 1. - self.alpha
         b1 = -2. * self.cs
@@ -138,8 +137,8 @@ class Allpass(_Biquad):
 
 class Peaking(_Biquad):
 
-    def __init__(self, freq, dbGain, srate, bandwidthOrQOrS, isBandwidth):
-        _Biquad.__init__(self,F.PEAKING,freq,dbGain,srate,bandwidthOrQOrS,isBandwidth)
+    def __init__(self, freq, dbGain, bandwidthOrQOrS,srate=44100, isBandwidth=False):
+        _Biquad.__init__(self,F.PEAKING,freq,dbGain,bandwidthOrQOrS,srate,isBandwidth)
 
         b0 = 1. + (self.alpha * self.A)
         b1 = -2. * self.cs
@@ -153,8 +152,8 @@ class Peaking(_Biquad):
 
 class Lowshelf(_Biquad):
 
-    def __init__(self, freq, dbGain, srate, bandwidthOrQOrS, isBandwidth):
-        _Biquad.__init__(self,F.LOW_SHELF,freq,dbGain,srate,bandwidthOrQOrS,isBandwidth)
+    def __init__(self, freq, dbGain, bandwidthOrQOrS, srate=44100):
+        _Biquad.__init__(self,F.LOW_SHELF,freq,dbGain,bandwidthOrQOrS,srate)
 
         b0 = self.A * ((self.A + 1.) - (self.A - 1.) * self.cs + self.beta)
         b1 = 2. * self.A * ((self.A - 1.) - (self.A + 1.) * self.cs)
@@ -168,8 +167,8 @@ class Lowshelf(_Biquad):
 
 class Highshelf(_Biquad):
 
-    def __init__(self, freq, dbGain, srate, bandwidthOrQOrS, isBandwidth):
-        _Biquad.__init__(self,F.HIGH_SHELF,freq,dbGain,srate,bandwidthOrQOrS,isBandwidth)
+    def __init__(self, freq, dbGain, bandwidthOrQOrS, srate=44100):
+        _Biquad.__init__(self,F.HIGH_SHELF,freq,dbGain,bandwidthOrQOrS,srate)
 
         b0 = self.A * ((self.A + 1.) + (self.A - 1.) * self.cs + self.beta)
         b1 = -2. * self.A * ((self.A - 1.) + (self.A + 1.) * self.cs)
@@ -181,8 +180,8 @@ class Highshelf(_Biquad):
         self._setCoef(array([b0/a0,b1/a0,b2/a0]),array([1.,a1/a0,a2/a0]))
 
 if __name__ == "__main__":
-    lp = Lowpass(100.0,10.0,44100,2.0,False)
-    lh = Highpass(100.0,10.0,44100,2.0,False)
+    lp = Lowpass(100.0,2.0)
+    lh = Highpass(100.0,2.0)
 
     filters = [lp,lh]
 
